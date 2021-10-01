cbc210f7420b3e110b4f83b4330a1b17e91743b6
Nick Battista <nickbattista@Nick.Battista>
2021-10-01 16:54:18 -0600

    adding to post routes and model

--- models/post.js
+++ models/post.js
@@ -3,6 +3,18 @@ const db = require("../db");
 
 class Post {
 
+    // static async create(post_id, event_id, user_id, post) {
+    //   const result = await db.query(
+    //       `INSERT INTO posts 
+    //       (post_id, event_id, user_id, post) 
+    //       VALUES ($1, $2, $3, $4)
+    //       RETURNING user_id, post`,
+    //       [post_id, event_id, user_id, post]
+    //   );
+    //   const post = result.rows[0];
+    //   return post;
+    // }
+
     static async findAll() {
         const result = await db.query(
               `SELECT post_id AS "postId",
@@ -15,6 +27,19 @@ class Post {
         return result.rows;
       }
 
+    static async get(id) {
+      const result = await db.query(
+            `SELECT post_id AS "postId",
+                    event_id AS "eventId",
+                    user_id AS "userId",
+                    post
+              FROM posts
+              WHERE event_id=$1`,
+              [id]
+      );
+  
+      return result.rows;
+    }
 }
 
 module.exports = Post;
--- routes/posts.js
+++ routes/posts.js
@@ -13,6 +13,25 @@ router.get('/', async (req, res) => {
 })
 
 // Get all event posts for specific event_id
+router.get("/:id", async function (req, res, next) {
+  try {
+    const posts = await Post.get(req.params.id);
+    return res.json({ posts });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Post new user post.
+router.post('/', async function(req, res, next) {
+  try {
+    const post = await Post.create(req.body)
+    return res.status(201).json({ post });
+  } catch (err) {
+    return next(err);
+  }
+})
+
  
 
 module.exports = router; 
