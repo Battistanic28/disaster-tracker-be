8c9dfafb21a24df75684775a44d15380a26ed0c5
Nick Battista <nickbattista@Nick.Battista>
2021-10-19 20:35:05 -0600

    added schema for signup and login forms

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const jsonschema = require("jsonschema");
 const jwt = require("jsonwebtoken");
 const {SECRET_KEY} = require("../config");
 const router = express.Router();
 const User = require("../models/user");
-
+const userRegistrationSchema = require("../schemas/userRegistrationSchema.json");
+const userAuthSchema = require("../schemas/userAuthSchema.json");
 
 // Register new user.
 router.post('/register', async (req, res, next) => {
     try {
+        const validator = jsonschema.validate(req.body, userRegistrationSchema);
+        if (!validator.valid) {
+          const errs = validator.errors.map(e => e.stack);
+          console.log(errs)
+        }
         const newUser = await User.register({ ...req.body, isAdmin: false});
         return res.status(201).json({newUser});
       } catch (err) {
@@ -16,13 +23,21 @@ router.post('/register', async (req, res, next) => {
   });
 
 router.post('/token', async (req, res, next) => {
-    const {username, password} = req.body;
-    const user = await User.authenticate(username, password);
-    const payload = {username: user.username, isAdmin: user.isAdmin}
-    const token = jwt.sign(payload, SECRET_KEY);
-
-    return res.json({token});
-})
+    try {
+        const validator = jsonschema.validate(req.body, userAuthSchema);
+        if (!validator.valid) {
+          const errs = validator.errors.map(e => e.stack);
+          console.log(errs);
+        }
+            const {username, password} = req.body;
+            const user = await User.authenticate(username, password);
+            const payload = {username: user.username, isAdmin: user.isAdmin}
+            const token = jwt.sign(payload, SECRET_KEY);
+            return res.json({token});
+      } catch (err) {
+        return next(err);
+    }
+  });
 
 
 module.exports = router;
