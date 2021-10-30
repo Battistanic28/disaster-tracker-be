851b2680cf6cbadac5bebd751f48dddc4d7ce33b
Nick Battista <nickbattista@Nick.Battista>
2021-10-30 14:03:54 -0600

    updated config file

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 const PORT = +process.env.PORT || 5000;
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-const DATABASE_URL = "natural_disasters";
-const SECRET_KEY = "ABC123";
+const DATABASE_URL = process.env.DATABASE_URL || "natural_disasters";
+const SECRET_KEY = process.env.SECRET_KEY || "ABC123";
 
 module.exports = {
     PORT,
